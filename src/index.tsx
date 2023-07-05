084daed6c656477758b52b1a5163519ac67539a9
Rafael Giro <me@rafaelgiro.dev>
2023-07-05 20:46:51 -0300

    feat: noralize css in styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Normalize } from './styles';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Normalize />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
