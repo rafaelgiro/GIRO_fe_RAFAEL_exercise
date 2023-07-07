5991df09567c1cd6d4690dced3fc77286c3fa7ae
Rafael Giro <me@rafaelgiro.dev>
2023-07-07 12:34:25 -0300

    feat: TeamOverview search test

--- src/components/Spinner/styles.ts
+++ src/components/Spinner/styles.ts
@@ -1,19 +1,19 @@
 import styled, { keyframes } from 'styled-components';
 
 export const spinnerAnimation = keyframes`
-from {
-        transform: rotate(0deg);
-    }
-    to {
-        transform: rotate(360deg);
-    }
+  from {
+      transform: rotate(0deg);
+  }
+  to {
+      transform: rotate(360deg);
+  }
 `;
 
 export const SpinnerBody = styled.div`
   height: 4rem;
   width: 4rem;
-  border: 4px solid #d1d5db;
-  border-top-color: #3b82f6;
+  border: 0.25rem solid ${({ theme }) => theme.primary200};
+  border-top-color: ${({ theme }) => theme.primary};
   border-radius: 50%;
   animation: ${spinnerAnimation} 800ms linear infinite;
 `;
--- src/pages/teams/__tests__/TeamOverview.spec.tsx
+++ src/pages/teams/__tests__/TeamOverview.spec.tsx
@@ -1,3 +1,8 @@
+import { rest } from 'msw';
+import ReactRouterDom from 'react-router-dom';
+
+import { API_URL } from '@/config';
+import { server } from '@/mocks/server';
 import { customRender, screen, waitFor } from '@/test/test-utils';
 
 import { TeamOverview } from '../TeamOverview';
@@ -15,12 +20,68 @@ jest.mock('react-router-dom', () => ({
   useSearchParams: () => [new URLSearchParams()],
 }));
 
-describe('TeamOverview', () => {
+describe('<TeamOverview />', () => {
   it('should render team overview users', async () => {
     customRender(<TeamOverview />);
 
     await waitFor(() => {
       expect(screen.getAllByRole('link')).toHaveLength(4);
     });
+  });
+});
+
+describe('<TeamOverview /> Search by name', () => {
+  beforeEach(() => {
+    jest
+      .spyOn(ReactRouterDom, 'useSearchParams')
+      .mockReturnValue([new URLSearchParams({ search: 'Lead' }), jest.fn()]);
+  });
+
+  it('should display only one user', async () => {
+    server.use(
+      rest.get(`${API_URL}/teams/1`, (_, res, ctx) => {
+        return res.once(
+          ctx.json({
+            id: 1,
+            teamLeadId: '2',
+            teamMemberIds: ['3'],
+          })
+        );
+      })
+    );
+
+    server.use(
+      rest.get(`${API_URL}/users/2`, (req, res, ctx) => {
+        return res.once(
+          ctx.json({
+            id: '2',
+            firstName: 'Lead',
+            lastName: 'Member',
+            displayName: 'displayLead',
+            location: 'Brazil',
+            avatar: '',
+          })
+        );
+      })
+    );
+
+    server.use(
+      rest.get(`${API_URL}/users/3`, (req, res, ctx) => {
+        return res.once(
+          ctx.json({
+            id: '3',
+            firstName: 'Team',
+            lastName: 'Member',
+            displayName: 'displayTeam',
+            location: 'Mexico',
+            avatar: '',
+          })
+        );
+      })
+    );
+
+    customRender(<TeamOverview />);
+
+    expect(await screen.findByRole('link')).toBeVisible();
   });
 });
